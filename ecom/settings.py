733e30c1db2b231fe25dc5312facdd5ebcfb1d82
cherryScripter <kreska323@gmail.com>
2024-12-07 22:59:30 +0100

    Added bucket

--- ecom/settings.py
+++ ecom/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'store',
     'payment',
     'paypal.standard.ipn',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['static/']
 
@@ -144,3 +146,21 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'cozyandkindled@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'cozyandkindled@gmail.com'
+
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'djangoecomproject'
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_SIGNATURE_NAME = 's3v4'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+
+# Media files storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# # Optional: to configure the URL for media files
+# MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+
