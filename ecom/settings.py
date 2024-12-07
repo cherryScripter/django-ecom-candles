173d3325a59773951a2f7a9330261efbd2544f86
cherryScripter <kreska323@gmail.com>
2024-12-07 23:37:05 +0100

    Public access to s3

--- ecom/settings.py
+++ ecom/settings.py
@@ -153,8 +153,9 @@ AWS_STORAGE_BUCKET_NAME = 'djangoecomproject'
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_SIGNATURE_NAME = 's3v4'
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_VERIFY = True
+
 
 # Media files storage
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
